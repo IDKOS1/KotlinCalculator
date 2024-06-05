bdb450f1847be06b8799c232d95d5e8be82c55bc
idkos <leegang31@gmail.com>
2024-06-05 19:42:38 +0900

    Lv2 완료

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -7,17 +7,24 @@ fun main() {
     println("두번째 숫자를 입력해 주세요.")
     var num2 = readLine()!!.toInt()
 
-    print("덧셈 결과: ")
-    println(Calculator().add(num1, num2))
-    print("뺄셈 결과: ")
-    println(Calculator().subtract(num1, num2))
-    print("곱셈 결과: ")
-    println(Calculator().multiply(num1, num2))
-    print("나눗셈 결과: ")
-    println(Calculator().divide(num1, num2))
+    var operation = 0
+    while(operation != -1) {
+        println("원하는 계산을 선택하세요")
+        println("1. 더하기  2. 빼기 3. 곱하기  4. 나누기  5. 나머지 구하기  -1. 종료")
+        operation = readLine()!!.toInt()
+        when(operation){
+            1 -> num1 =Calculator().add(num1, num2)
+            2 -> num1 =Calculator().subtract(num1, num2)
+            3 -> num1 =Calculator().multiply(num1, num2)
+            4 -> num1 =Calculator().divide(num1, num2)
+            5 -> num1 = Calculator().mod(num1, num2)
+            -1 -> return println("종료합니다.")
+        }
+        println(num1)
+    }
 }
 
-class Calculator {
+class Calculator{
 
     fun add(num1: Int, num2: Int): Int {
         return num1 + num2
@@ -28,7 +35,10 @@ class Calculator {
     fun multiply(num1: Int, num2: Int): Int {
         return num1 * num2
     }
-    fun divide(num1: Int, num2: Int): Double {
-        return num1.toDouble() / num2.toDouble()
+    fun divide(num1: Int, num2: Int): Int {
+        return num1 / num2
+    }
+    fun mod(num1: Int, num2: Int): Int{
+        return num1 % num2
     }
 }
