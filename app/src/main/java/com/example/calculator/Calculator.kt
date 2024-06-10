6582f5e760d73a38789e1d26031b1f6a23856179
idkos <leegang31@gmail.com>
2024-06-10 10:41:21 +0900

    Lv4 수정 및 완료

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -12,11 +12,12 @@ fun main() {
         println("1. 더하기  2. 빼기 3. 곱하기  4. 나누기  5. 나머지 구하기  -1. 종료")
         operation = readLine()!!.toInt()
         if(operation == -1) return println("종료합니다.")
+        var addOperate = AddOperation()
         when(operation){
-            1 -> num1 = AddOperation().operate(num1, num2)
-            2 -> num1 = SubstractOperation().operate(num1, num2)
-            3 -> num1 = MultiplyOperation().operate(num1, num2)
-            4 -> num1 = DivideOperation().operate(num1, num2)
+            1 -> num1 = Calculator(AddOperation()).operate(num1, num2)
+            2 -> num1 = Calculator(SubtractOperation()).operate(num1, num2)
+            3 -> num1 = Calculator(MultiplyOperation()).operate(num1, num2)
+            4 -> num1 = Calculator(DivideOperation()).operate(num1, num2)
             -1 -> return println("종료합니다.")
             else -> return println("잘못 입력했습니다. 종료합니다.")
         }
@@ -25,10 +26,13 @@ fun main() {
     }
 }
 
-abstract class AbstractOperation {
-    init {
-        println("계산을 진행합니다...")
+class Calculator(private val operation: AbstractOperation) {
+    fun operate(num1: Int, num2: Int): Int {
+        return operation.operate(num1, num2)
     }
+}
+
+abstract class AbstractOperation {
     abstract fun operate(num1: Int, num2: Int): Int
 }
 
@@ -39,7 +43,7 @@ class AddOperation: AbstractOperation() {
     }
 }
 
-class SubstractOperation: AbstractOperation() {
+class SubtractOperation: AbstractOperation() {
     override fun operate(num1: Int, num2: Int): Int {
         return num1 - num2
     }
